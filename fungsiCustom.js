1dc3d3335aae47b4184356a3e8d49a41a0c5fffe
adimas_r_b <rengga.adimas@gmail.com>
2022-06-12 12:15:10 +0700

    challengeAdvancedCallback06RevisiLagi

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -19,27 +19,35 @@ let modifyFile3 = (val) => {
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
 const bacaData = (fnCallback) => {
-  let index1 = "";
-  let index2 = "";
-  let index3 = "";
-  let temp = [];
-
-  fs.readFile(file1, 'utf-8', (err, data1) => {    
-  fs.readFile(file2, 'utf-8', (err, data2) => { 
-  fs.readFile(file3, 'utf-8', (err, data3) => {
-    data = [
-      index1 = JSON.parse(data1)['message'].split(" ")[1],
-      index2 = JSON.parse(data2)[0]['message'].split(" ")[1],
-      index3 = JSON.parse(data3)[0]['data']['message'].split(" ")[1]
-    ];
-    temp.push(index1);
-    temp.push(index2);
-    temp.push(index3);
-
-    fnCallback(err, temp);
-  });    
+  let temp = []
+  fs.readFile(file2,'utf8', (err, data) => {
+    if (err) fnCallback(err,null);
+    temp.push(jsonType2(data))
+  fs.readFile(file1,'utf8', (err, data) => {
+    if (err) fnCallback(err,null);
+    temp.push(jsonType1(data))
+  fs.readFile(file3,'utf8', (err, data) => {
+    if (err) fnCallback(err,null);
+    temp.push(jsonType3(data))
+    
+    fnCallback(null, temp);
+  });
   });
 });
+};
+const jsonType1 = (j) => {
+    let obj = JSON.parse(j);
+    return (obj.message.split(" ")[1]);
+}
+
+const jsonType2 = (j) => {
+  let obj = JSON.parse(j);
+  return (obj[0].message.split(" ")[1]);
+}
+
+const jsonType3 = (j) => {
+  let obj = JSON.parse(j);
+  return (obj[0].data.message.split(" ")[1]);
 }
 
 // ! JANGAN DIMODIFIKASI
